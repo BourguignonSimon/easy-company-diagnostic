9e4790b9c20ca2ce04abd5b064852986290e43c9
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-27 10:26:54 +0100

    Fix: Simplify main entry point to display questionnaire directly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import DiagnosticForm from './components/DiagnosticForm';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className="min-h-screen bg-gray-50 py-8">
+      <DiagnosticForm />
+    </div>
+  </React.StrictMode>,
 );
