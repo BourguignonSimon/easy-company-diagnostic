1185de340bb3e2ebabcf2c599baca0b16efaffa8
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-27 15:03:34 +0100

    Mise à jour des routes pour supporter les nouveaux questionnaires

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
+import QuestionnaireForm from './components/QuestionnaireForm'
 import './index.css'
-
-// Import des composants de diagnostic
-import DiagnosticIndividuel from './components/DiagnosticIndividuel'
-import DiagnosticOrganisationnel from './components/DiagnosticOrganisationnel'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/diagnostic-individuel" element={<DiagnosticIndividuel />} />
-        <Route path="/diagnostic-organisationnel" element={<DiagnosticOrganisationnel />} />
+        <Route path="/diagnostic-rapide" element={<QuestionnaireForm type="rapide_strategique" />} />
+        <Route path="/diagnostic-individuel" element={<QuestionnaireForm type="individuel_approfondi" />} />
+        <Route path="/diagnostic-organisationnel" element={<QuestionnaireForm type="organisationnel_complet" />} />
       </Routes>
     </Router>
   </React.StrictMode>
