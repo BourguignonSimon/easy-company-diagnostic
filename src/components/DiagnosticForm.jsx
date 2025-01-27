1b0a4c4eace5332fc1a068a90bf833bf96a755ac
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-27 10:35:42 +0100

    Ajout du composant DiagnosticForm manquant

---
+++ src/components/DiagnosticForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DiagnosticForm = () => {
+  return (
+    <div className="diagnostic-form">
+      <h2>Diagnostic Organisationnel</h2>
+      <form>
+        {/* Formulaire de diagnostic à compléter */}
+        <p>Formulaire de diagnostic en cours de développement</p>
+      </form>
+    </div>
+  );
+};
+
+export default DiagnosticForm;
