2fc5a1b539375fc7fe5da1088fac0b5573e625a7
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-28 10:05:52 +0100

    Configuration des routes principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
 import DiagnosticLayout from './components/DiagnosticLayout';
-import {
-  OrganisationnelDiagnostic,
-  LeadershipDiagnostic,
-  SatisfactionDiagnostic,
-  CommunicationDiagnostic
-} from './pages/diagnostics';
+import Home from './pages/Home';
+import DiagnosticPage from './pages/diagnostics';
+import ResultatPage from './pages/resultat';
 
 const App = () => {
   return (
@@ -15,11 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/diagnostic" element={<DiagnosticLayout />}>
-          <Route path="organisationnel" element={<OrganisationnelDiagnostic />} />
-          <Route path="leadership" element={<LeadershipDiagnostic />} />
-          <Route path="satisfaction" element={<SatisfactionDiagnostic />} />
-          <Route path="communication" element={<CommunicationDiagnostic />} />
+          <Route path=":type" element={<DiagnosticPage />} />
         </Route>
+        <Route path="/resultat" element={<ResultatPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App'
-import QuestionnaireForm from './components/QuestionnaireForm'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/diagnostic-rapide" element={<QuestionnaireForm type="rapide_strategique" />} />
-        <Route path="/diagnostic-individuel" element={<QuestionnaireForm type="individuel_approfondi" />} />
-        <Route path="/diagnostic-organisationnel" element={<QuestionnaireForm type="organisationnel_complet" />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
-)
+    <App />
+  </React.StrictMode>,
+);
