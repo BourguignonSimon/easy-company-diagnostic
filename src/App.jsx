25745c0853c74a8d7ce86472fbe449b4d026e1fe
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-29 21:04:49 +0100

    Mise à jour du composant App pour garder le menu identique pendant la navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import DiagnosticForm from './components/DiagnosticForm';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/diagnostic/:type" element={<DiagnosticForm />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </Layout>
