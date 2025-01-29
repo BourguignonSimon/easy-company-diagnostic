12fac03f287ecbce231943e4f64484e8736e834f
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-29 21:10:32 +0100

    Création de la page DiagnosticRapide

---
+++ src/pages/DiagnosticRapide.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DiagnosticForm from '../components/DiagnosticForm';
+
+const DiagnosticRapide = () => {
+  return (
+    <DiagnosticForm type="rapide" />
+  );
+};
+
+export default DiagnosticRapide;
