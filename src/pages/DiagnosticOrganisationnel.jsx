cb680fe413bcfb31673b32600b3eae973ca55137
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-29 21:10:40 +0100

    Création de la page DiagnosticOrganisationnel

---
+++ src/pages/DiagnosticOrganisationnel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DiagnosticForm from '../components/DiagnosticForm';
+
+const DiagnosticOrganisationnel = () => {
+  return (
+    <DiagnosticForm type="organisationnel" />
+  );
+};
+
+export default DiagnosticOrganisationnel;
