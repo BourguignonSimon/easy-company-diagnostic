acca3f0381d3350e195f7543e34503f74a42ebcc
BourguignonSimon <bourguignon.simon@gmail.com>
2025-01-29 21:10:36 +0100

    Création de la page DiagnosticIndividuel

---
+++ src/pages/DiagnosticIndividuel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DiagnosticForm from '../components/DiagnosticForm';
+
+const DiagnosticIndividuel = () => {
+  return (
+    <DiagnosticForm type="individuel" />
+  );
+};
+
+export default DiagnosticIndividuel;
